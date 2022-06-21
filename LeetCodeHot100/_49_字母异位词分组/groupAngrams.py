dcdfdfecee9c68b5b9717a5dd6b89b0d2230f27f
lichong0309 <chongli0309@gmail.com>
2022-06-21 12:51:39 +0800

    update

--- LeetCodeHot100/_49_字母异位词分组/groupAngrams.py
+++ LeetCodeHot100/_49_字母异位词分组/groupAngrams.py
@@ -25,57 +25,66 @@ class Solution:
     
     
     
-    
-# class Solution:
-#     def groupAnagrams(self, strs):
-#         if len(strs) == 0:
-#             return [[""]]
-#         else:
-#             ans = []
-#             # {count:[[],[]]}
-#             dic = {}
+### Solution2 #####   
+class Solution:
+    def groupAnagrams(self, strs):
+        if len(strs) == 0:
+            return [[""]]
+        else:
+            ans = []
+            # {count:[[],[]]}
+            dic = {}
 
-#             # 计算列表中每个元素的ASCII的值
-#             for i in range(len(strs)):
-#                 count = 0 
-#                 for j in strs[i]:
-#                     count = count + ord(j)
+            # 计算列表中每个元素的ASCII的值
+            for i in range(len(strs)):
+                count = 0 
+                for j in strs[i]:
+                    count = count + ord(j)
 
-#                 # 如果字典中已经存在ASCII的值
-#                 # 判断strs[i]在什么位置
-#                 if count in dic:
-#                     flag = 0          # 判断strs[i]是否找到应该插入位置的标志，0表示没有，1表示找到了
-#                     # 遍历dic[count]中每个子list
-#                     for m in dic[count]:
-#                         temp = m[0]     # 随机选择子list中的一个元素
-#                         # 判断子list中的元素和strs[i]的元素是否相同
-#                         if len(temp) == len(strs[i]):
-#                             flag_1 = 0   # 判断temp和strs[i]中元素是否全部相当，0表示全部相等，1表示不全部相等
-#                             for w in temp:
-#                                 if w not in list(strs[i]):
-#                                     flag_1 = 1
-#                                 else:
-#                                     pass
-#                             if flag_1 == 0:         # 如果temp和strs[i]全部相等
-#                                 m.append(strs[i])
-#                                 flag = 1
-#                                 break       # strs[i]找到了位置，则退出对每个子list的循环
-#                             else:           # 如果temp和strs[i]不全部相等
-#                                 pass
-#                         else:       # temp和strs[i]的长度都不相同，直接pass
-#                             pass
+                # 如果字典中已经存在ASCII的值
+                # 判断strs[i]在什么位置
+                if count in dic:
+                    flag = 0          # 判断strs[i]是否找到应该插入位置的标志，0表示没有，1表示找到了
+                    # 遍历dic[count]中每个子list
+                    for m in dic[count]:
+                        # temp = m[0]     # 随机选择子list中的一个元素
+                        # # 判断子list中的元素和strs[i]的元素是否相同
+                        # if len(temp) == len(strs[i]):
+                        #     # flag_1 = 0   # 判断temp和strs[i]中元素是否全部相当，0表示全部相等，1表示不全部相等
+                        #     if sorted(temp) == sorted(strs[i]):
+                        #         flag_1 = 0
+                        #     else:
+                        #         flag_1 = 1
+                                
+                        #     if flag_1 == 0:         # 如果temp和strs[i]全部相等
+                        #         m.append(strs[i])
+                        #         flag = 1
+                        #         break       # strs[i]找到了位置，则退出对每个子list的循环
+                        #     else:           # 如果temp和strs[i]不全部相等
+                        #         pass
+                        # else:       # temp和strs[i]的长度都不相同，直接pass
+                        #     pass
+                        temp = m[0]        # 随机选择子list中的一个元素
+                        # 先对temp 和 strs[i] 进行排序，如果排序完之后相等，则temp 和 strs[i]的元素相同
+                        if sorted(temp) == sorted(strs[i]):
+                            m.append(strs[i])
+                            flag = 1        # flag=1 表示找到了位置
+                            break           # 退出循环
+                        else:
+                            pass
+                            
 
-#                     if flag == 1:       # 找到了位置
-#                         pass
-#                     else:               # 没有找到位置
-#                         dic[count].append([strs[i]])     # 则开辟新的子list给strs[i]
+                    if flag == 1:       # 找到了位置
+                        pass
+                    else:               # 没有找到位置
+                        dic[count].append([strs[i]])     # 则开辟新的子list给strs[i]
                 
-#                 # 如果字典中不存在ASCII的值        
-#                 else:       
-#                     dic[count] = [[strs[i]]]             # 则开辟新的子list给strs[i]
+                # 如果字典中不存在ASCII的值        
+                else:       
+                    dic[count] = [[strs[i]]]             # 则开辟新的子list给strs[i]
             
-#             for key, value in dic.items():
-#                 for sub_list in value:
-#                     ans.append(sub_list)
+            for key, value in dic.items():
+                for sub_list in value:
+                    ans.append(sub_list)
         
-#             return ans
+            return ans
