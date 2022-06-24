7258c15d7103cf99e3eeef1e92e3b7d118bfee06
lichong0309 <chongli0309@gmail.com>
2022-06-24 23:36:32 +0800

    update

--- LeetCodeHot100/_215_数组中的第K个最大元素/findKthLargest.py
+++ LeetCodeHot100/_215_数组中的第K个最大元素/findKthLargest.py
@@ -2,6 +2,6 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # nums.sort(reverse=True)
-        # result = nums[k-1]
-        # return result
+        nums.sort(reverse=True)
+        result = nums[k-1]
+        return result
---
+++ LeetCodeHot100/_347_前 K 个高频元素/topKFrequent.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # # dic{key,value}
+        # # key: 元素
+        # # value: 元素的数量
+        # dic = collections.Counter(nums)
+        
+        # # 按照value排序,从小到大 ->List[tuple(int, int)]
+        # dicSort = sorted(dic.items(), key= lambda x:[x[1], x[0]], reverse=True)
+
+        # ans = []
+        # count = 1
+        # while count <= k:
+        #     ans.append(dicSort[count-1][0])
+        #     count += 1
+
+        # return ans
+
+        freq = collections.Counter(nums)
+
+        return [f[0] for f in freq.most_common(k)]
