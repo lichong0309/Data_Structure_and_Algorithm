c116bdb9b26ea1a207da5305b5d6c2da4a63148d
lichong0309 <chongli0309@gmail.com>
2022-09-03 17:43:50 +0800

    update

---
+++ Tencent_50/_121_买卖股票的最佳时机/maxProfit.py
@@ -0,0 +1,59 @@
+'''
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+
+你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+示例 1：
+输入：[7,1,5,3,6,4]
+输出：5
+解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+     
+示例 2：
+输入：prices = [7,6,4,3,1]
+输出：0
+解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+ 
+提示：
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+'''
+
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        # 思路：
+        #   1） 得到某天和前一天股票的价差，存放到profit数组中
+        #   2） 找到最大子数组和，类似于第53题
+        # 时间复杂度：O(n), n为prices数组的长度
+        
+        length = len(prices)
+
+
+        profit = [0] * length
+        profit[0] = -prices[0]          # 初始化第一天的股票为负数
+
+        for i in range(1, length):
+            profit[i] = prices[i] - prices[i-1]
+        
+        
+        
+        dp = [0] * length
+        dp[0] = profit[0]               # 初始化第一天的dp[0]为profit[0]
+
+        for i in range(1, length):
+            if dp[i-1] >= 0:
+                dp[i] = profit[i] + dp[i-1]
+            else:
+                dp[i] = profit[i]
+        
+        
+        ans = max(dp)
+        
+        if ans >= 0:
+            return ans
+        else:
+            return 0
