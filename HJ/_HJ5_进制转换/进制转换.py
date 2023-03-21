ba2e6259afb3ce268ec2dbf43f25aa0c344b0e60
lichong0309 <chongli0309@gmail.com>
2023-03-21 14:20:28 +0800

    update

--- HJ/_HJ5_进制转换/进制转换.py
+++ HJ/_HJ5_进制转换/进制转换.py
@@ -2,12 +2,15 @@ class Solution(object):
     def __init__(self):
         pass
     def solution(self, s):
-        pass
+        return int(s, 16)
 
     
 if __name__ == "__main__":
     while True:
         try:
-            pass
+            s = input().strip()
+            slt = Solution()
+            ans = slt.solution(s)
+            print(ans)
         except EOFError:
             break
